9fd5a2e929f5c07f770ea5df833ed5baba0ca5ef
THIAGODENNY <THIAGODENNY1@GMAIL.COM>
2017-10-19 23:48:14 -0200

    Add files via upload

---
+++ Exercicio_Reposicao/01.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+
+int main()
+{
+	int a[30],c[30];
+	char b[30];
+	float d[30];
+	cout<<"********************************************************"<<endl;
+	cout<<"Utilize apenas os operadores + - / *"<<endl;
+	cout<<"Coloque a expressão de forma direta. Ex:1+1,33/4..."<<endl;
+	for(int i=0;i<30;i++){
+		cout<<endl<<"Calc"<<i+1<<":";
+		cin>>a[i]>>b[i]>>c[i];
+		
+		//definir as operações
+		if(b[i]=='+'){
+			d[i]=(float)a[i]+(float)c[i];
+		}
+		else if(b[i]=='-'){
+			d[i]=(float)a[i]-(float)c[i];
+		}
+		else if(b[i]=='/'){
+			d[i]=(float)a[i]/(float)c[i];
+		}
+		else if(b[i]=='*'){
+			d[i]=(float)(a[i])*(float)(c[i]);
+		}
+		//operação inválida não grava vetor e refaz
+		else{
+			cout<<"Operação Inválida";
+			i--;
+			continue;
+		}
+
+		
+	}
+	
+	for(int i=0;i<30;i++){
+		cout<<endl<<"Calc"<<i+1<<":";
+		fixed;
+		cout<<a[i]<<b[i]<<c[i]<<"="<<setprecision(3)<<d[i];
+	}
+
+	cout<<endl;
+	return 0;		
+
+}
