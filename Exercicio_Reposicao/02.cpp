a0f1793e45542b4db2bf89901dca6b45bf5d9596
THIAGODENNY <THIAGODENNY1@GMAIL.COM>
2017-10-20 00:10:56 -0200

    Add files via upload

---
+++ Exercicio_Reposicao/02.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<iomanip>
+#include <locale.h>
+
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	int elemento[15];
+	bool elemento_30_encontrado=false;
+
+	for(int i=0;i<15;i++){
+		cout<<"Elemento "<<i+1<<":";
+		cin>>elemento[i];
+	}
+
+	for(int i=0;i<15;i++){
+
+		if(elemento[i]==30){
+			cout<<endl<<"Elemento "<<i+1<<":"<<elemento[i];
+			elemento_30_encontrado=true;
+		}
+	}
+
+	if(elemento_30_encontrado==false){
+		cout<<"Nenhum número 30 foi encontrado";
+	}
+
+	cout<<endl;
+	return 0;
+}
