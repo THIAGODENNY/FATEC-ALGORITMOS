f46c16890cc6a019073f8eef9df61d86313f413a
THIAGODENNY <THIAGODENNY1@GMAIL.COM>
2017-10-24 21:01:41 -0200

    Update 03.cpp

--- Exercicio_Reposicao/03.cpp
+++ Exercicio_Reposicao/03.cpp
@@ -2,24 +2,35 @@
 #include<iomanip>
 #include <locale.h>
 
-using namespace std;
+#define quantidadeVendedores 2
 
+using namespace std;
 
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
 	
-	float codVendedor[10],totalVendas[10],comissao[10],valorReceber[10];
+	float codVendedor[quantidadeVendedores],totalVendas[quantidadeVendedores],comissao[quantidadeVendedores],valorReceber[quantidadeVendedores];
 	
-	for(int i=0;i<10;i++){
+	for(int i=0;i<quantidadeVendedores;i++){
 	    cout<<endl<<"Digite o código do Vendedor "<<i+1<<":";
 	    cin>>codVendedor[i];
 	    cout<<"Digite o total vendido por ele:";
 	    cin>>totalVendas[i];
 	    cout<<"Digite a comissão dele(%):";
 	    cin>>comissao[i];
-	    valorReceber[i]=totalVendas[i]*(comissao[i]/100);
+	    valorReceber[i]=(float)totalVendas[i]*((float)comissao[i]/100);
 	}
 	
+	cout<<endl<<"Relatório dos vendedores e vendas:"<<endl;
+	
+	for(int i=0;i<quantidadeVendedores;i++){
+		cout<<"Cod Vendedor:"<<codVendedor[i]<<endl;
+	    std::fixed;
+		cout<<"Total Vendas: R$"<<setprecision(5)<<(float)totalVendas[i]<<endl;
+	    cout<<"Comissão:"<<comissao[i]<<"%"<<endl;
+	    std::fixed;
+	    cout<<"Total Comissão: R$"<<setprecision(5)<<(float)valorReceber[i]<<endl<<endl;
+	}	
 	
 }
