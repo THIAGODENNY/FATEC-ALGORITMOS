df9395a6c38f112ebedc86a71e0f72217bb1df45
THIAGODENNY <THIAGODENNY1@GMAIL.COM>
2017-10-31 12:34:40 -0200

    Create 04.cpp
    Parado no ponto de criação de rotina de interseccção

---
+++ Exercicio_Reposicao/04.cpp
@@ -0,0 +1,32 @@
+/*Escrever um programa para ler dois vetores de inteiros, de ordem n e m respectivamente (n  15 e m  25). Calcular o vetor
+interseção dos dois vetores dados, imprimido os três vetores. Se não houver a interseção, imprimir uma mensagem explicativa
+adequada.*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define n 15
+#define m 25
+
+int main()
+{
+  
+  int vetor1[n],vetor2[m],vetorIntersecao[n];
+  
+  //entrar com os valores do vetor 01
+  for(int i=0;i<n;i++){
+      cout<<"Vetor 01,"<<i<<":";
+      cin>>vetor1[i];
+  }
+  
+  //entrar com os valores do vetor 02
+  for(int i=0;i<m;i++){
+      cout<<"Vetor 02,"<<i<<":";
+      cin>>vetor2[i];
+  }
+  
+  //verificar interseccção*********************************
+  
+}
