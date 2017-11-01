3a22863a99b1580c7b3f8815d28aa48cd1a4f33d
THIAGODENNY <THIAGODENNY1@GMAIL.COM>
2017-11-01 01:39:35 -0200

    Create 08.cpp

---
+++ Exercicio_Reposicao/08.cpp
@@ -0,0 +1,15 @@
+/*Escrever um programa para ler um vetor de n elementos do tipo inteiro (n  20) e ordenar seus elementos em ordem crescente e
+decrescente. Imprimir o vetor antes e após a ordenação.*/
+
+#include<iostream>
+#include<locale.h>
+
+using namespace std;
+
+#define n 10
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+
+}
