2b0e276337315dfbcef33c22bbf7c8c1542ae3e8
THIAGODENNY <THIAGODENNY1@GMAIL.COM>
2017-11-02 16:29:56 -0200

    Update 08.cpp

--- Exercicio_Reposicao/08.cpp
+++ Exercicio_Reposicao/08.cpp
@@ -6,10 +6,128 @@ decrescente. Imprimir o vetor antes e após a ordenação.*/
 
 using namespace std;
 
-#define n 10
+#define n 20
 
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
+	
+	int vetor[n],vetorCrescente[n],vetorDescrescente[n],vetorAux[n],indiceAtual=0,numeroMaior=0,numeroMenor=0,indiceMaior=0,indiceMenor=0,trocaElementoVetor;
+	
+	cout<<"Capturando dados.."<<endl;
 
+	//zerando vetores
+	for(int i=0;i<n;i++){
+		vetor[i]=0;
+		vetorCrescente[i]=0;
+		vetorDescrescente[i]=0;
+	}
+
+	//capturando dados
+	for(int i=0;i<n;i++){
+		cout<<"Vetor 1,"<<i+1<<":";
+		cin>>vetor[i];
+		vetorCrescente[i]=vetor[i];
+		vetorDescrescente[i]=vetor[i];
+	}
+
+	
+
+//************************************ Classificação Forma descrescente *******************************************//
+	
+	
+	//movendo vetor para vetorAux
+	for(int i=0;i<n;i++){
+		vetorAux[i]=vetor[i];
+	}
+
+	//inicializando numeroMenor para classificação em forma decrescente
+	numeroMaior=vetor[0];
+	indiceMaior=0;
+	indiceAtual=0;
+
+	//organizando em forma decrescente e colocando em vetorAux
+	for(int i=0;i<n;i++){
+		for(int j=indiceAtual;j<n;j++){
+			if(numeroMaior<vetorAux[j]){
+				numeroMaior=vetorAux[j];
+				indiceMaior=j;
+			}
+		}
+			
+		//troca a posição do elemento atual pelo a do elemento maior
+		trocaElementoVetor=vetorAux[indiceMaior];
+		vetorAux[indiceMaior]=vetorAux[indiceAtual];
+		vetorAux[indiceAtual]=trocaElementoVetor;
+		//incrementa a posição do vetor Auxiliar
+		indiceAtual++;
+		numeroMaior=vetorAux[indiceAtual];
+		indiceMaior=indiceAtual;		
+	}
+
+	//movendo vetorAux para vetor decrescente
+	for(int i=0;i<n;i++){
+		vetorDescrescente[i]=vetorAux[i];
+	}
+
+//************************************ Classificação Forma crescente *********************************************//
+
+	//movendo vetor para vetorAux
+	for(int i=0;i<n;i++){
+		vetorAux[i]=vetor[i];
+	}
+	
+	//inicializando numeroMenor para classificação em forma crescente
+	numeroMenor=vetor[0];
+	indiceMenor=0;
+	indiceAtual=0;
+
+	//organizando em forma crescente e colocando em vetorAux
+	for(int i=0;i<n;i++){
+		for(int j=indiceAtual;j<n;j++){
+			if(numeroMenor>vetorAux[j]){
+				numeroMenor=vetorAux[j];
+				indiceMenor=j;
+			}
+		}
+			
+		//troca a posição do elemento atual pelo a do elemento maior
+		trocaElementoVetor=vetorAux[indiceMenor];
+		vetorAux[indiceMenor]=vetorAux[indiceAtual];
+		vetorAux[indiceAtual]=trocaElementoVetor;
+		//incrementa a posição do vetor Auxiliar
+		indiceAtual++;
+		numeroMenor=vetorAux[indiceAtual];
+		indiceMenor=indiceAtual;		
+	}
+
+	//movendo vetorAux para vetor crescente
+	for(int i=0;i<n;i++){
+		vetorCrescente[i]=vetorAux[i];
+	}
+
+//*****************************************************************************************************************
+
+
+	cout<<"Vetores resultantes:"<<endl;
+	
+	cout<<endl<<"Vetor Inicial:"<<endl;
+	//imprimindo vetor
+	for(int i=0;i<n;i++){
+		cout<<"\t"<<vetor[i];
+	}
+
+	cout<<endl<<"Vetor Inicial em ordem Crescente:"<<endl;
+	//imprimindo vetor
+	for(int i=0;i<n;i++){
+		cout<<"\t"<<vetorCrescente[i];
+	}
+
+	cout<<endl<<"Vetor Inicial em ordem Decrescente:"<<endl;
+	//imprimindo vetor
+	for(int i=0;i<n;i++){
+		cout<<"\t"<<vetorDescrescente[i];
+	}
+
+	cout<<endl<<endl;	
 }
