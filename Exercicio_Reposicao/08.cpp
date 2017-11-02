6faa8ca02a963e7e087a70e9d3fa2711f2952bf7
THIAGODENNY <THIAGODENNY1@GMAIL.COM>
2017-11-02 16:58:17 -0200

    Update 08.cpp
    Correção de bug ao digitar um caractere na entrada de dados.

--- Exercicio_Reposicao/08.cpp
+++ Exercicio_Reposicao/08.cpp
@@ -13,6 +13,7 @@ int main()
 	setlocale(LC_ALL, "Portuguese");
 	
 	int vetor[n],vetorCrescente[n],vetorDescrescente[n],vetorAux[n],indiceAtual=0,numeroMaior=0,numeroMenor=0,indiceMaior=0,indiceMenor=0,trocaElementoVetor;
+	bool erroCin=false;
 	
 	cout<<"Capturando dados.."<<endl;
 
@@ -26,7 +27,18 @@ int main()
 	//capturando dados
 	for(int i=0;i<n;i++){
 		cout<<"Vetor 1,"<<i+1<<":";
-		cin>>vetor[i];
+			cin>>vetor[i];
+
+		//verifica se foi digitado na entrada que não for int, causando um erro no programa e pede para a inserção do valor novamente
+		while (cin.fail())  
+		{
+			cout<<"Caracter digitado inválido. Digite novamente o vetor 1,"<<i+1<<":";  
+				cin.clear();   				//limpa istream
+				cin.ignore();  				//repara istream
+				cin >> vetor[i]; 			//executa novamente a captura de dados
+		}
+		
+		//copia os valores para vetor crescente e decrescente
 		vetorCrescente[i]=vetor[i];
 		vetorDescrescente[i]=vetor[i];
 	}
